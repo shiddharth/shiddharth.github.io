00f855488e7faa1582b06dcbdeba66f222a0911d
shiddharth <67194380+shiddharth@users.noreply.github.com>
2020-08-29 18:27:32 +0600

    Made some changes to the static files.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-/*===== MENU SHOW =====*/ 
+/*===== SHOW MENU =====*/ 
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
@@ -15,11 +15,11 @@ showMenu('nav-toggle','nav-menu')
 const navLink = document.querySelectorAll('.nav__link');   
 
 function linkAction(){
-  /*Active link*/
+  /*ACTIVE*/
   navLink.forEach(n => n.classList.remove('active'));
   this.classList.add('active');
   
-  /*Remove menu mobile*/
+  /*REMOVE (MOBILE)*/
   const navMenu = document.getElementById('nav-menu')
   navMenu.classList.remove('show')
 }
@@ -33,24 +33,24 @@ const sr = ScrollReveal({
     reset: false
 });
 
-/*SCROLL HOME*/
+/*SCROLL: HOME*/
 sr.reveal('.home__title',{}); 
 sr.reveal('.button',{delay: 200}); 
 sr.reveal('.home__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 190}); 
 
-/*SCROLL ABOUT*/
+/*SCROLL: ABOUT*/
 sr.reveal('.about__img',{}); 
 sr.reveal('.about__subtitle',{delay: 400}); 
 sr.reveal('.about__text',{delay: 400}); 
 
-/*SCROLL SKILLS*/
+/*SCROLL: SKILLS*/
 sr.reveal('.skills__subtitle',{}); 
 sr.reveal('.skills__text',{}); 
 sr.reveal('.skills__data',{interval: 190}); 
 sr.reveal('.skills__img',{delay: 600});
 
-/*SCROLL CERTIFICATIONS*/
+/*SCROLL: CERTIFICATIONS*/
 sr.reveal('.certs__img',{interval: 190}); 
 
 
