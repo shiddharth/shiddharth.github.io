48067f72acc88b160355c0a8b8c5c488493be5ce
Anindya Shiddhartha <67194380+shiddharth@users.noreply.github.com>
2021-04-21 01:19:59 +0600

    Completely revamped the website.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-/*===== SHOW MENU =====*/ 
+/*===== SHOW MENU =====*/
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
@@ -12,13 +12,13 @@ const showMenu = (toggleId, navId) =>{
 showMenu('nav-toggle','nav-menu')
 
 /*===== ACTIVATE AND REMOVE MENU =====*/
-const navLink = document.querySelectorAll('.nav__link');   
+const navLink = document.querySelectorAll('.nav__link');
 
 function linkAction(){
   /*ACTIVE*/
   navLink.forEach(n => n.classList.remove('active'));
   this.classList.add('active');
-  
+
   /*REMOVE (MOBILE)*/
   const navMenu = document.getElementById('nav-menu')
   navMenu.classList.remove('show')
@@ -34,24 +34,24 @@ const sr = ScrollReveal({
 });
 
 /*SCROLL: HOME*/
-sr.reveal('.home__title',{}); 
-sr.reveal('.button',{delay: 200}); 
-sr.reveal('.home__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 190}); 
+sr.reveal('.home__title',{});
+sr.reveal('.button',{delay: 200});
+sr.reveal('.home__img',{delay: 400});
+sr.reveal('.home__social-icon',{ interval: 190});
 
 /*SCROLL: ABOUT*/
-sr.reveal('.about__img',{}); 
-sr.reveal('.about__subtitle',{delay: 400}); 
-sr.reveal('.about__text',{delay: 400}); 
+sr.reveal('.about__img',{});
+sr.reveal('.about__subtitle',{delay: 400});
+sr.reveal('.about__text',{delay: 400});
 
 /*SCROLL: SKILLS*/
-sr.reveal('.skills__subtitle',{}); 
-sr.reveal('.skills__text',{}); 
-sr.reveal('.skills__data',{interval: 190}); 
+sr.reveal('.skills__subtitle',{});
+sr.reveal('.skills__text',{});
+sr.reveal('.skills__data',{interval: 190});
 sr.reveal('.skills__img',{delay: 600});
 
 /*SCROLL: PROJECTS*/
-sr.reveal('.projects__img',{interval: 190}); 
+sr.reveal('.projects__img',{interval: 190});
 
 /*SCROLL: CERTIFICATIONS*/
-sr.reveal('.certs__img',{interval: 190}); 
+sr.reveal('.certs__img',{interval: 190});
